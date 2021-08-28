dfc64a5e9a72ea4c4f7dbc85a80e15ed8663c0c0
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-28 18:57:31 -0300

    theme: add base styles for input

--- src/theme.ts
+++ src/theme.ts
@@ -61,5 +61,51 @@ export const theme = extendTheme({
         variant: 'solid',
       },
     },
+
+    Input: {
+      baseStyle: {
+        field: {
+          outline: 'none',
+          color: 'main.primary',
+          boxShadow: 'none',
+        },
+      },
+
+      variants: {
+        filled: () => ({
+          field: {
+            border: '2px solid',
+            borderColor: 'transparent',
+            borderRadius: '2xl',
+            bg: 'grays.100',
+
+            _hover: {
+              bg: 'grays.100',
+            },
+
+            _focus: {
+              bg: 'grays.200',
+              borderColor: 'none',
+              boxShadow: 'none',
+
+              color: 'main.primary',
+            },
+          },
+        }),
+      },
+
+      sizes: {
+        md: {
+          field: {
+            h: 16,
+          },
+        },
+      },
+
+      defaultProps: {
+        size: 'md',
+        variant: 'filled',
+      },
+    },
   },
 })
