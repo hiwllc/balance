10cfd16477fe87d18b685ab6487c2971ee58f96e
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-27 22:51:37 -0300

    styles: add theme with colors

---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    main: {
+      primary: '#FE3E6D',
+      secondary: '#1A93DA',
+    },
+
+    fills: {
+      blue: '#EAF7FF',
+    },
+
+    grays: {
+      100: '#F0F4F8',
+      200: '#DEE4E9',
+      300: '#C7CBCF',
+      400: '#6B7076',
+      500: '#3B3B3B',
+    }
+  }
+})
