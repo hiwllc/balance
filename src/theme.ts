43f364499ea73052daafbd71ded947f6f09a9d25
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-28 20:52:45 -0300

    theme: add table base styles

--- src/theme.ts
+++ src/theme.ts
@@ -107,5 +107,61 @@ export const theme = extendTheme({
         variant: 'filled',
       },
     },
+
+    Table: {
+      variants: {
+        simple: {
+          th: {
+            px: 0,
+            py: 6,
+            borderBottom: 'none',
+            fontSize: 'sm',
+            fontWeight: 'normal',
+            textTransform: 'initial',
+            color: 'grays.400',
+
+            _first: { pl: 4 },
+            _last: { pr: 4 },
+          },
+
+          tbody: {
+            my: 2,
+            rounded: '2xl',
+            boxShadow: '0 0 0 1px #C7CBCF',
+            border: 'none',
+            borderColor: 'gray.100',
+          },
+
+          td: {
+            px: 0,
+            border: 'none',
+
+            fontSize: 'md',
+            color: 'grays.400',
+
+            _first: { pl: 4 },
+            _last: { pr: 4 },
+          },
+
+          tr: {
+            _first: {
+              '& td': {
+                pt: 7,
+              },
+            },
+
+            _last: {
+              '& td': {
+                pb: 7,
+              },
+            },
+          },
+        },
+      },
+
+      defaultProps: {
+        variant: 'simple',
+      },
+    },
   },
 })
