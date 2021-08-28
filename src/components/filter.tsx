06d91a234644e49bf2c82f5c7eb8f106e68dd2a0
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-28 19:06:35 -0300

    components: add filter

---
+++ src/components/filter.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@chakra-ui/button'
+import { HStack } from '@chakra-ui/layout'
+import { Search } from './search'
+
+export const Filter = () => (
+  <HStack spacing={7}>
+    <Button variant="active">Tudo</Button>
+    <Button>Entrada</Button>
+    <Button>Saida</Button>
+    <Button>Futuro</Button>
+
+    <Search />
+  </HStack>
+)
