e79f4d398deb9e7a4b7f45c92a70f497e183d9ee
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-29 19:23:40 -0300

    app: add empty state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,19 @@ import { Filter } from '../components/filter'
 import { Table } from '../components/table'
 import { TableLoader } from '../components/loader'
 import { useBalance } from '../hooks/useBalance'
+import { Box, Text } from '@chakra-ui/layout'
 
 export default function Home() {
-  const { transactions, filter, onFilterChange, onQueryChange, isLoading } =
-    useBalance({
-      filter: 'all',
-    })
+  const {
+    transactions,
+    filter,
+    onFilterChange,
+    onQueryChange,
+    isLoading,
+    isError,
+  } = useBalance({
+    filter: 'all',
+  })
 
   return (
     <>
@@ -16,6 +23,19 @@ export default function Home() {
         onTypeChange={onFilterChange}
         onQueryChange={onQueryChange}
       />
+
+      {(transactions.length || isError) <= 0 ? (
+        <Box py={6}>
+          <Text
+            textAlign="center"
+            fontSize="lg"
+            fontWeight="700"
+            color="main.primary"
+          >
+            Nenhuma transação encontrada.
+          </Text>
+        </Box>
+      ) : null}
 
       {isLoading ? <TableLoader /> : <Table data={transactions} />}
     </>
