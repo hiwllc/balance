ce2fbaf1478610db5743437d09623d1e0a186fef
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-29 18:21:48 -0300

    filter: refactor use balance hooks to handle with filter

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -1,20 +1,81 @@
+import { useMemo, useState } from 'react'
 import { useQuery } from 'react-query'
 import { fetchBalance } from '../services/balance'
-import { Transactions } from '../types'
+import { DayTransactions, Transaction, Filter } from '../types'
 
-function orderByNewest(transactions: Transactions['results'] = []) {
+type UseFilter = {
+  filter: Filter
+}
+
+function orderByNewest(transactions: DayTransactions[] = []) {
   // @ts-expect-error left-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint'
   return transactions.sort((a, b) => new Date(b.date) - new Date(a.date))
 }
 
-export function useBalance() {
+function removeEmptyTransactions(transactions: DayTransactions[]) {
+  return transactions.filter(({ items }) => Boolean(items.length))
+}
+
+function getIncomeTransactions(transactions: Transaction[]) {
+  return transactions.filter(transaction => transaction.entry === 'CREDIT')
+}
+
+function getOutcomeTransactions(transactions: Transaction[]) {
+  return transactions.filter(transaction => transaction.entry === 'DEBIT')
+}
+
+function getFutureTransactions(transactions: Transaction[]) {
+  return transactions.filter(transaction => Boolean(transaction.scheduled))
+}
+
+export function useBalance({ filter }: UseFilter) {
   const { data, isLoading, isError } = useQuery('GET_BALANCE', () =>
     fetchBalance()
   )
+  const [keyword, setKeyword] = useState(filter)
+
+  const onFilterChange = (filter: Filter) => setKeyword(filter)
+
+  const ordered = useMemo(() => orderByNewest(data?.results), [data])
+
+  const transactionFiltered = useMemo(() => {
+    if (keyword === 'income') {
+      const filtered = ordered.map(trx => ({
+        ...trx,
+        items: getIncomeTransactions(trx.items),
+      }))
+
+      return removeEmptyTransactions(filtered)
+    }
+
+    if (keyword === 'outcome') {
+      const filtered = ordered.map(trx => ({
+        ...trx,
+        items: getOutcomeTransactions(trx.items),
+      }))
+
+      return removeEmptyTransactions(filtered)
+    }
+
+    if (keyword === 'future') {
+      const filtered = ordered.map(trx => ({
+        ...trx,
+        items: getFutureTransactions(trx.items),
+      }))
+
+      return removeEmptyTransactions(filtered)
+    }
+
+    return ordered
+  }, [ordered, keyword])
+
+  const results = useMemo(() => transactionFiltered, [transactionFiltered])
 
   return {
-    data: { ...data, results: orderByNewest(data?.results) },
+    onFilterChange,
+    transactions: results,
     isLoading,
     isError,
+    filter: keyword,
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import { Filter } from '../components/filter'
 import { Table } from '../components/table'
 import { useBalance } from '../hooks/useBalance'
-import { useFilter } from '../hooks/useFilter'
 
 export default function Home() {
-  const { data } = useBalance()
-  const {
-    data: transactions,
-    onFilterChange,
-    filter,
-  } = useFilter({
-    initialState: data.results,
+  const { transactions, filter, onFilterChange } = useBalance({
     filter: 'all',
   })
 
