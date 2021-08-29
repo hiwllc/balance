0b181ba3141e1d4e5f78a78d9a17b3b8940915a9
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-28 22:27:12 -0300

    api: add react-query to handle with api requests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,36 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider, Grid } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import { theme } from '../theme'
 import { Header } from '../components/header'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
+      <QueryClientProvider client={queryClient}>
+        <Header />
 
-      <Grid
-        as="main"
-        px={4}
-        gridTemplateColumns="1fr min(1035px, 100%) 1fr"
-        sx={{ '& > *': { gridColumn: 2 } }}
-      >
-        <Component {...pageProps} />
-      </Grid>
+        <Grid
+          as="main"
+          px={4}
+          gridTemplateColumns="1fr min(1035px, 100%) 1fr"
+          sx={{ '& > *': { gridColumn: 2 } }}
+        >
+          <Component {...pageProps} />
+        </Grid>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
