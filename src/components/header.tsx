d1d211e2be1d8d662bc66a584e1c2a4937b35147
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-08-30 19:52:50 -0300

    header: fix linter errors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,16 @@
 import { Grid, Text } from '@chakra-ui/react'
 
 export const Header = () => (
-  <Grid as="header" px={4} py={6} borderBottomWidth="1px" borderBottomColor="grays.200" gridTemplateColumns="1fr min(1035px, 100%) 1fr">
-    <Text color="grays.500" fontSize="xl" fontWeight="bold" gridColumn={2}>Extrato</Text>
+  <Grid
+    as="header"
+    px={4}
+    py={6}
+    borderBottomWidth="1px"
+    borderBottomColor="grays.200"
+    gridTemplateColumns="1fr min(1035px, 100%) 1fr"
+  >
+    <Text color="grays.500" fontSize="xl" fontWeight="bold" gridColumn={2}>
+      Extrato
+    </Text>
   </Grid>
 )
